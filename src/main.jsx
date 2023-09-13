5be2194ec5f1c26a45bd8b62579c28e4836ab6f7
Muhammad Auzair <78294230+Nobody12here@users.noreply.github.com>
2023-09-13 17:17:29 +0500

    added wallet connect

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import {
   ThirdwebProvider,
   metamaskWallet,
   coinbaseWallet,
+  walletConnect
 } from "@thirdweb-dev/react";
 import { createTheme } from "@mui/material/styles";
-import { BinanceTestnet } from "@thirdweb-dev/chains";
 import ThemeStyles from "./assets/styles/ThemeStyles.jsx";
 import GlobalStyles from "./assets/styles/GlobalStyles.jsx";
 const theme = createTheme(ThemeStyles);
@@ -17,7 +17,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThirdwebProvider
-      supportedWallets={[metamaskWallet(), coinbaseWallet()]}
+      supportedWallets={[metamaskWallet(), coinbaseWallet(),walletConnect()]}
       activeChain={"binance"}
       clientId="809b2c588a04312ead2eab0c92d1d81c"
     >
